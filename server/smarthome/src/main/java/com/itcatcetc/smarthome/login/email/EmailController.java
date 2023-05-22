71a76c33008646696d4411e1e745606500b4849e
Breinich <bajnokvencel@edu.bme.hu>
2023-05-22 22:04:11 +0200

    add authorization and validation to email service

--- server/smarthome/src/main/java/com/itcatcetc/smarthome/login/email/EmailController.java
+++ server/smarthome/src/main/java/com/itcatcetc/smarthome/login/email/EmailController.java
@@ -1,20 +1,24 @@
 package com.itcatcetc.smarthome.login.email;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path="api/v1/smarthome/email")
 public class EmailController {
 
     @Autowired
     private EmailService emailService;
 
     // Sending a simple Email
-    @PostMapping("/sendMail")
-    public String
-    sendMail(@RequestBody EmailDetails details)
+    @PostMapping
+    @PreAuthorize("hasRole('GUEST') or hasRole('HOMIE')")
+    public String sendMail(@Valid @RequestBody EmailDetails details)
     {
         return emailService.sendSimpleMail(details);
     }
--- server/smarthome/src/main/java/com/itcatcetc/smarthome/login/email/EmailDetails.java
+++ server/smarthome/src/main/java/com/itcatcetc/smarthome/login/email/EmailDetails.java
@@ -1,6 +1,7 @@
 package com.itcatcetc.smarthome.login.email;
 
 
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class EmailDetails {
-
+    @Email
     private String recipient;
     private String msgBody;
     private String subject;
